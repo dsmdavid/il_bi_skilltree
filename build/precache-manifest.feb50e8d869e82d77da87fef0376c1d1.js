8314835473269dcde87cb20ce1144126e5cef83b
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2021-06-23 15:24:43 +0100

    Remove build from gitignore

---
+++ build/precache-manifest.feb50e8d869e82d77da87fef0376c1d1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d98174cfa00d85af2520714d1ed97b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf7a645e4dc2fa3470fd",
+    "url": "/static/css/main.3ebb4ef3.chunk.css"
+  },
+  {
+    "revision": "a22addbfe59b61a37e5f",
+    "url": "/static/js/2.b1aa1efa.chunk.js"
+  },
+  {
+    "revision": "cf7a645e4dc2fa3470fd",
+    "url": "/static/js/main.dd3113c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "941b1a878935311ff34a3e9f8186b495",
+    "url": "/static/media/Calculateicon.941b1a87.svg"
+  },
+  {
+    "revision": "177539ec55f588663f1ffcf8b527c9c5",
+    "url": "/static/media/Excludeicon.177539ec.svg"
+  },
+  {
+    "revision": "e0ef5c32830fe30ebfe169343fdf87ae",
+    "url": "/static/media/Ideicon.e0ef5c32.svg"
+  },
+  {
+    "revision": "52cc5b7a87bbb6bd36c84aeb8a6fc455",
+    "url": "/static/media/Lodicon.52cc5b7a.svg"
+  },
+  {
+    "revision": "a65c26179ba925504a2d97b392ed4de2",
+    "url": "/static/media/Parametericon.a65c2617.svg"
+  },
+  {
+    "revision": "bce39ea2521d54c6493868606a080edb",
+    "url": "/static/media/Seticon.bce39ea2.svg"
+  },
+  {
+    "revision": "ab3ef22d96e6a69c7819f67a8cc94ddd",
+    "url": "/static/media/Tableauactionicon.ab3ef22d.svg"
+  },
+  {
+    "revision": "8992ddb7b1f1f6814caa86a5e6d13069",
+    "url": "/static/media/Tableauadminicon.8992ddb7.svg"
+  },
+  {
+    "revision": "e60314dd68de676db939acd6709ee440",
+    "url": "/static/media/Tableauapiicon.e60314dd.svg"
+  },
+  {
+    "revision": "b1ae4a92856f6a196a96e58048733b85",
+    "url": "/static/media/Tableauautomateicon.b1ae4a92.svg"
+  },
+  {
+    "revision": "8e678b4e58b4b23e47e1cc0b9325b852",
+    "url": "/static/media/Tableaucontenticon.8e678b4e.svg"
+  },
+  {
+    "revision": "9b7bf6af50f4aa87f92c24341750ca2a",
+    "url": "/static/media/Tableaudocumenticon.9b7bf6af.svg"
+  },
+  {
+    "revision": "ed7c63c9ac77e589fc0a783774ed4bf9",
+    "url": "/static/media/Tableauicon.ed7c63c9.svg"
+  },
+  {
+    "revision": "ee287292234efbbe581b000905e9541a",
+    "url": "/static/media/Tableaumetadataicon.ee287292.svg"
+  },
+  {
+    "revision": "44b5b842d3a72bccba6d56815491abcf",
+    "url": "/static/media/Tableaupermissionicon.44b5b842.svg"
+  },
+  {
+    "revision": "8446b909c5d805c9b072c67dd4a58633",
+    "url": "/static/media/Tableauprojecticon.8446b909.svg"
+  },
+  {
+    "revision": "7dc3e93782aa1b5cfd041c9a3f06fcea",
+    "url": "/static/media/Tableaurestapiicon.7dc3e937.svg"
+  },
+  {
+    "revision": "28b517d9c4516b02bf731007730ab839",
+    "url": "/static/media/Tableauscheduleicon.28b517d9.svg"
+  },
+  {
+    "revision": "e0b656e58ac7f50ec51b143442dee02f",
+    "url": "/static/media/Tableauserverclienticon.e0b656e5.svg"
+  },
+  {
+    "revision": "30e48b746db5339e970bcf72bc891342",
+    "url": "/static/media/Tableauservericon.30e48b74.svg"
+  },
+  {
+    "revision": "6a8a0abc4d72a4fb42d2b2dc7375aea4",
+    "url": "/static/media/Tableauwebhookicon.6a8a0abc.svg"
+  },
+  {
+    "revision": "5caadee4413837d5d3b1c4d5d84100ab",
+    "url": "/static/media/Windowicon.5caadee4.svg"
+  },
+  {
+    "revision": "ef0f131dc89280a6a12a66cbab516ad1",
+    "url": "/static/media/logo.ef0f131d.svg"
+  }
+]);
